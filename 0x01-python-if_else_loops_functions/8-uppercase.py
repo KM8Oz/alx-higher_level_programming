27b1c7d8d981d5b3df78fa4c2463c38d26e23a5a
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-06-07 20:18:23 +0100

    0x01. Python - if/else, loops, functions: first commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,5 @@
 def uppercase(string):
     for char in string:
         uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
-        print(uppercase_char, end='')    
-print()
+        print(uppercase_char, end='')
+    print()
