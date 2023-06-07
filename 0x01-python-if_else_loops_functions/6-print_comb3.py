701bd2f555cb28d1f4817a90012e61cc3b1686be
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-06-08 00:34:53 +0100

    0x01. Python - if/else, loops, functions: first commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(9):
-    for n in range(num, 10):
+    for n in range(num-1, 10):
         if (num*10+n) != 89:
             print("{:d}{:d},".format(num, n), end=' ')
         else:
