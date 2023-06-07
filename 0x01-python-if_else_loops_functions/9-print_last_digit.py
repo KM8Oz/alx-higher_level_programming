e6f4f7f0f80c63c32225aeac541bdc92cbc7fc29
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-06-08 00:45:02 +0100

    0x01. Python - if/else, loops, functions: first commit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,5 @@ def print_last_digit(number):
     last_digit = abs(number) % 10
     if number < 0:
         last_digit *= -1
-    print("{:d}".format(last_digit))
+    print("{:d}".format(last_digit), end='')
     return "{:d}".format(last_digit)
