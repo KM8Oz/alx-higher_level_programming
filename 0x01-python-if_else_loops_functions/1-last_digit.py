bb57cc3a4f88d8e609deca1e6eeab8e90fe7c322
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-06-07 20:20:35 +0100

    0x01. Python - if/else, loops, functions: first commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if (abs(number) % 10) > 5:
     print(f"""
     Last digit of {number:d} is
-     {(abs(number) % 10):d} and is greater than 5 and not 0
+     {(abs(number) % 10):d} and is greater than 5
     """)
 else:
     if (abs(number) % 10) != 0:
