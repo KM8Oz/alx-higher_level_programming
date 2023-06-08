50e5d3c92e5ae4d9e20f29a758b3c759886db54e
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-06-08 01:07:13 +0100

    0x01. Python - if/else, loops, functions: first commit

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
     return "{:.0f}".format(a ** b) if b >= 0 else "{:.10g}".format(a ** b)
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
+print(pow(10, -2))
+print(pow(-89, -10))
