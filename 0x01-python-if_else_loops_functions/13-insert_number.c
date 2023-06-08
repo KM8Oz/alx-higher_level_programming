be621b3ba1e98fa8c8a597d8c349b31139fc97d0
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-06-08 01:28:00 +0100

    0x01. Python - if/else, loops, functions: first commit

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,7 +1,33 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    last_digit = abs(number) % 10
-    if number < 0:
-        last_digit *= -1
-    print("{:d}".format(abs(last_digit)), end='')
-    return "{:d}".format(abs(last_digit))
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to pointer of the head of the list
+ * @number: the number to be inserted
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL) {
+		return NULL;  // Failed to allocate memory for the new node
+	}
+
+	newNode->n = number;
+	newNode->next = NULL;
+
+	if (*head == NULL || number < (*head)->n) {
+		newNode->next = *head;
+		*head = newNode;
+	} else {
+		listint_t *current = *head;
+		while (current->next != NULL && number > current->next->n) {
+			current = current->next;
+		}
+		newNode->next = current->next;
+		current->next = newNode;
+	}
+
+	return newNode;
+}
