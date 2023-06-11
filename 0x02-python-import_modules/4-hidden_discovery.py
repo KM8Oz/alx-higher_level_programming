7622e9cc2416f30f0b8d70021eb617805329c771
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-06-11 05:35:26 +0100

    0x02-python-import_modules: commit 3

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,4 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     temp = dir(hidden_4)
-    [[temp[i][j] == '_' and temp[i][j+1] == '_' or print(temp[i]) for j in range(len(temp[i]))] for i in range(len(temp))]
+    [[temp[i][j] == '_' and temp[i][j+1] == '_' or print(temp[i])
+        for j in range(len(temp[i]))] for i in range(len(temp))]
