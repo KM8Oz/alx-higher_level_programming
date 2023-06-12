096797c242faa4f84bf0944e9b4154588222c7ff
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-06-12 23:27:38 +0100

    0x03-python-data_structures: commit 3

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    try:
-        if idx >= 0:
-            return my_list[idx]
-        else:
-            return None
-    except Exception as ex:
+    if idx >= 0 and idx < len(my_list):
+        return my_list[idx]
+    else:
         return None
