0baa0397ae0ca54d24ec20be42bc1d447beb83aa
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-06-13 03:41:06 +0100

    0x03-python-data_structures: commit 8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    [[(print("{:d}".format(j[1]), end='') or ((j[0] != 2 or print()) and print(
-        " ", end=''))) for j in enumerate(sub)] for sub in matrix]
+    if matrix != [[]]:
+        _ = [[(print("{:d}".format(j[1]), end='') or ((j[0] != 2 or print()) and print(
+            " ", end=''))) for j in enumerate(sub)] for sub in matrix]
+    else:
+        print('')
