febba6caf07ccc59b52d9bc6295e958d71d5700b
KM8Oz <ouazmir@gmail.com>
2023-06-17 03:05:05 +0100

    0x04-python-more_data_structures: commit 3

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return [{**a_dictionary, key: a_dictionary.get(key)*2} for key in a_dictionary][-1]
+    new = a_dictionary.copy()
+    [new.update({key: a_dictionary.get(key)*2}) for key in a_dictionary]
+    return new
